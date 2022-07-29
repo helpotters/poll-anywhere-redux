e2857872b2cad3f3041223ae4a6fb30ab34fbd31
Paul Lemus <paullemus@protonmail.com>
2022-07-28 17:43:19 -0700

    feat: poll form submission passing
    Feature test for submitting a poll is passing. Now we can setup the rest
    of the bare skeleton.

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -2,4 +2,24 @@ class PollsController < ApplicationController
   def new
     @poll = Poll.new
   end
+
+  def edit; end
+
+  def create
+    @poll = Poll.create(poll_params)
+
+    respond_to do |format|
+      if @poll.save
+        format.html { render :edit, notice: 'Poll created' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def poll_params
+    params.require(:poll).permit(:title)
+  end
 end
--- spec/features/create_poll_spec.rb
+++ spec/features/create_poll_spec.rb
@@ -8,8 +8,11 @@ RSpec.feature 'Poll Interface', type: :feature do
   end
   feature 'Poll Creation' do
     scenario 'Host creates a new poll' do
-      fill_in('Poll Title', with: Faker::Cosmere.spren) # Stormlight reference is essential
+      title = Faker::Cosmere.spren
+      fill_in('Poll Title', with: title) # Stormlight reference is essential
       click_button('Submit')
+
+      expect(page).to have_content(title)
     end
   end
 end
