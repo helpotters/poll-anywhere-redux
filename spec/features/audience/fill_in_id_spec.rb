fc611984a3af78cc8e582350cf0b077341e891f8
Paul Lemus <paullemus@protonmail.com>
2022-08-01 19:34:57 -0700

    spec: find by id audience feature spec

--- spec/features/audience/fill_in_id_spec.rb
+++ spec/features/audience/fill_in_id_spec.rb
@@ -7,7 +7,11 @@ RSpec.feature 'Audience', type: :feature do
     visit root_path
   end
   feature 'Joining' do
-    xscenario 'Join Poll as Audience Member' do
+    let(:poll) { create(:poll) }
+    scenario 'Join Poll as Audience Member' do
+      fill_in('Join by ID', with: poll.id)
+      click_button('Join')
+      expect(page).to have_content(poll.title)
     end
     xscenario 'Join Expired Poll'
   end
