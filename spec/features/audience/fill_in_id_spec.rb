713f65db9f2b4f5d15b01e7af42c771d4de215fb
Paul Lemus <paullemus@protonmail.com>
2022-08-04 14:28:26 -0700

    spec: adding name requirement for voter model

--- spec/features/audience/fill_in_id_spec.rb
+++ spec/features/audience/fill_in_id_spec.rb
@@ -10,8 +10,11 @@ RSpec.feature 'Audience', type: :feature do
     let(:poll) { create(:poll) }
     scenario 'Join Poll as Audience Member' do
       fill_in('Join by ID', with: poll.id)
+      name = Faker::Cosmere.herald
+      fill_in('Name', with: name)
       click_button('Join')
       expect(page).to have_content(poll.title)
+      expect(page).to have_content(name)
     end
     xscenario 'Join Expired Poll'
   end
