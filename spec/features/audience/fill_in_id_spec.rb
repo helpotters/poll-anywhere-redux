2f8a14c9d367710976bfcd3a73df29533dbe3ac0
Paul Lemus <paullemus@protonmail.com>
2022-08-04 16:06:51 -0700

    refactor: move join poll to helper method

--- spec/features/audience/fill_in_id_spec.rb
+++ spec/features/audience/fill_in_id_spec.rb
@@ -6,17 +6,20 @@ RSpec.feature 'Audience', type: :feature do
   before do
     visit root_path
   end
+  def join_poll(poll)
+    fill_in('Join by ID', with: poll.id)
+    name = Faker::Cosmere.herald
+    fill_in('Name', with: name)
+    click_button('Join')
+  end
   feature 'Joining' do
     let(:poll) { create(:poll) }
     scenario 'Join Poll as Audience Member' do
-      fill_in('Join by ID', with: poll.id)
-      name = Faker::Cosmere.herald
-      fill_in('Name', with: name)
-      click_button('Join')
+      join_poll(poll)
       expect(page).to have_content(poll.title)
       expect(page).to have_content(name)
     end
     xscenario 'Join Expired Poll'
   end
-  xfeature 'Responding'
+  feature 'Responding'
 end
