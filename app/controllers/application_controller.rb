91f186effb14e1243da32f5e1ca1204379ebb000
Paul Lemus <paullemus@protonmail.com>
2022-08-05 06:42:43 -0700

    feat: added rescue to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   include UserSessionsHelper
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
+
+  def record_not_found
+    redirect_to root_path
+  end
 
   def logged_in_user
     unless been_here_before?
