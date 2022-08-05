c35c5c98bff9fe57565ef3ebde8fcce3f426fd3d
Paul Lemus <paullemus@protonmail.com>
2022-08-04 17:18:08 -0700

    spec: removed vote requirement
    Vote count is settled by the option model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should validate_presence_of(:vote) }
   it { should belong_to(:poll) }
   it { should belong_to(:option) }
   it { should belong_to(:user) }
