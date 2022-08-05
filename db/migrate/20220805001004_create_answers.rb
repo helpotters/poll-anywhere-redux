b584a42f89c014419de473e31a44c59599d54e63
Paul Lemus <paullemus@protonmail.com>
2022-08-04 17:13:34 -0700

    feat: moved user migration in with answers

--- db/migrate/20220805001004_create_answers.rb
+++ db/migrate/20220805001004_create_answers.rb
@@ -1,5 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration[7.0]
   def change
+    create_table :users do |t|
+      t.text :name
+
+      t.timestamps
+    end
     create_table :answers do |t|
       t.integer :vote_count
       t.references :poll, null: false, foreign_key: true
