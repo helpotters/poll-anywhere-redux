4a9f3667d43c48b930a9c0a4252a3d1bf6eae8eb
Paul Lemus <paullemus@protonmail.com>
2022-08-04 17:04:14 -0700

    fix: poll helper bug

--- spec/features/audience/fill_in_id_spec.rb
+++ spec/features/audience/fill_in_id_spec.rb
@@ -3,23 +3,32 @@
 require 'rails_helper'
 
 RSpec.feature 'Audience', type: :feature do
+  let(:poll) { create(:poll) }
   before do
     visit root_path
   end
-  def join_poll(poll)
+  def join_poll(poll, name)
     fill_in('Join by ID', with: poll.id)
-    name = Faker::Cosmere.herald
     fill_in('Name', with: name)
     click_button('Join')
   end
   feature 'Joining' do
-    let(:poll) { create(:poll) }
     scenario 'Join Poll as Audience Member' do
-      join_poll(poll)
+      name = Faker::Cosmere.herald
+      join_poll(poll, name)
       expect(page).to have_content(poll.title)
       expect(page).to have_content(name)
     end
     xscenario 'Join Expired Poll'
   end
-  feature 'Responding'
+  feature 'Responding' do
+    scenario 'Audience Member Submits Valid Vote' do
+      name = Faker::Cosmere.herald
+      join_poll(poll, name)
+      click_button('Vote')
+      # find first option.click
+      # submit
+      # expect that option to have one vote in visualization view
+    end
+  end
 end
